94573aa06c95b82f7ded28b098c7c8e71707ae0a
Stefano Salasco <stefano.salasco@gmail.com>
2018-07-09 15:08:57 +0000

    added SimpleStock class

--- ISIN-ticker.py
+++ ISIN-ticker.py
@@ -14,15 +14,15 @@ def requestIsin(isin):
                 dati= json.dumps(jDes)
                 print(dati)
                 req = requests.post(url, data=dati, headers=header)
-                #print(req.json())
-                c = 0
-                for l in req.json()[0]['data']:
-                    if l['exchCode'] == isin[c][:2]:
-                        c += 1
+                req_json = req.json()[0]['data']
+                print(req_json)
+                for l in req_json:
+                    #print(l)
+                    if l['exchCode'] == i[:2]:
                         ticker = l['ticker']
                         resp.append(ticker)
                         print(resp)
     except FileNotFoundError as fnf:
         print("{} . Il file specificato non esiste.".format(fnf))
 
-print(requestIsin(["US0378331005", "US88160R10134"]))
+print(requestIsin(["US0378331005", "US88160R1014"]))
